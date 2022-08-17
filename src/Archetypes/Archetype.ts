ee8ba475f32cd2adf013c8d3d09ef4dd42e13b6b
SheilaNS <shei.nsantos@gmail.com>
2022-08-17 01:09:11 -0300

    feat(Requisito 05): :sparkles: Requisito finalizado
    Classes Mage, Necromancer, Ranger e Warrior criadas

--- src/Archetypes/Archetype.ts
+++ src/Archetypes/Archetype.ts
@@ -4,12 +4,10 @@ export default abstract class Archetype {
   private _name: string;
   private _special: number;
   private _cost: number;
-  private static _instanceCount = 0;
   constructor(name: string) {
     this._name = name;
     this._special = 0;
     this._cost = 0;
-    Archetype._instanceCount += 1;
   }
 
   get name() {
@@ -25,11 +23,7 @@ export default abstract class Archetype {
   }
 
   static createdArchetypeInstances(): number {
-    try {
-      return Archetype._instanceCount;
-    } catch (error) {
-      throw new Error('Not implemented');
-    }
+    throw new Error('Not implemented');
   }
 
   abstract get energyType(): EnergyType;
